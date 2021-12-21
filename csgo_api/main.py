d9bc0f5fa91b89cf53c1953c5143900b22c03773
Reni Dantas <renidantas0@gmail.com>
2021-12-21 18:22:23 -0300

    fix!: breaking the code, just for fun

--- csgo_api/main.py
+++ csgo_api/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, json, jsonify, request
 from flask_restful import reqparse, abort, Api, Resource
 from flask_cors import CORS
-from .rcon import Client
+from rcon import Client
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
